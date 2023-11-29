01165b7626712f5f129b22ff3b73d7f6defb812d
aquamariny <142547171+aquamariny@users.noreply.github.com>
2023-11-29 15:49:28 -0700

    Update GameMode.java

--- GameMode.java
+++ GameMode.java
@@ -1,18 +1,10 @@
 
-/**
- * Contains the main method and calls methods to initialize the game
- */
 
 public class GameMode{
 
-    /** integer representing the lenght of the longest ship*/
     final static int BATTLESHIP = 5;    
     
-    /**
-     * Main method, initializes the game.
-     * 
-     * @param   args    array of strings
-     */
+
     
     public static void main (String args[])
     {        
