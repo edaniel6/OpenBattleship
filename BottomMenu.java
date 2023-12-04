8ca1d53bdb18d4a1eae664746dccd4091870f940
aquamariny <142547171+aquamariny@users.noreply.github.com>
2023-12-04 15:54:54 -0700

    Update BottomMenu.java

--- BottomMenu.java
+++ BottomMenu.java
@@ -6,13 +6,17 @@ import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Dimension;
 import java.awt.Color;
-import java.awt.Point;
 
 //panel for displaying game play information
 
 public class BottomMenu extends JPanel
 {
-    //number of guesses made
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	//number of guesses made
     int numberOfGuesses = 0;
     
     //JLabel containing the integer numberOfGuesses
@@ -29,29 +33,29 @@ public class BottomMenu extends JPanel
     }
     
     //panel with the necessary information
-    public void paintComponent(Graphics g)
+    public void paintComponent(Graphics graphics)
     {
-        super.paintComponent(g);
+        super.paintComponent(graphics);
         setBackground(Color.black);
-        g.setColor(Color.black);
-        g.drawRect(0,0,399,99);
-        g.fillRect(5,15,10,10);
-        g.drawString("Miss",20,25);
-        g.setColor(Color.green);
-        g.fillRect(5,30,10,10);
-        g.setColor(Color.white);
-        g.drawString("Patrol Boat (2)",20,40);
-        g.setColor(Color.blue);
-        g.fillRect(5,45,10,10);
-        g.setColor(Color.white);
-        g.drawString("Submarine (3)",20,55);
-        g.setColor(Color.red);
-        g.fillRect(5,60,10,10);
-        g.setColor(Color.white);
-        g.drawString("Battleship (4)",20,70);
-        g.setColor(Color.yellow);
-        g.fillRect(5,75,10,10);
-        g.setColor(Color.white);
-        g.drawString("Carrier (5)",20,85);        
+        graphics.setColor(Color.black);
+        graphics.drawRect(0,0,399,99);
+        graphics.fillRect(5,15,10,10);
+        graphics.drawString("Miss",20,25);
+        graphics.setColor(Color.green);
+        graphics.fillRect(5,30,10,10);
+        graphics.setColor(Color.white);
+        graphics.drawString("Patrol Boat (2)",20,40);
+        graphics.setColor(Color.blue);
+        graphics.fillRect(5,45,10,10);
+        graphics.setColor(Color.white);
+        graphics.drawString("Submarine (3)",20,55);
+        graphics.setColor(Color.red);
+        graphics.fillRect(5,60,10,10);
+        graphics.setColor(Color.white);
+        graphics.drawString("Battleship (4)",20,70);
+        graphics.setColor(Color.yellow);
+        graphics.fillRect(5,75,10,10);
+        graphics.setColor(Color.white);
+        graphics.drawString("Carrier (5)",20,85);        
     }    
 }
