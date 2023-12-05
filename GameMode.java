3d874647f105c58247deba3efcae284e21a62a96
aquamariny <142547171+aquamariny@users.noreply.github.com>
2023-12-04 20:53:15 -0700

    Update GameMode.java

--- GameMode.java
+++ GameMode.java
@@ -6,11 +6,13 @@ public class GameMode{
     final static int BATTLESHIP = 5;    
     
     // main method, initializes the game
+
+    
     public static void main (String args[])
     {        
     	MainGUI gameBoard = new MainGUI();       
         
-    	gameBoard.eraseBoard();        
+    	gameBoard.clearBoard();        
 
         for (int i = 2; i <= BATTLESHIP; i++)
         {
