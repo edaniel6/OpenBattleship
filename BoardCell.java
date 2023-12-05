648a852a952c20126ac7f7fc130d811e8761428d
aquamariny <142547171+aquamariny@users.noreply.github.com>
2023-12-04 20:54:04 -0700

    Update BoardCell.java

--- BoardCell.java
+++ BoardCell.java
@@ -6,9 +6,9 @@ import java.awt.Graphics;
 //JPanel that will be placed on each possible guess position
 public class BoardCell extends JPanel
 {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    //constructor
+	//constructor
     public BoardCell()
     {
         setPreferredSize(new Dimension(50,50));
